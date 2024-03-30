b6fda7ec1ee890ea15d257c269e1e1bd3bf8aa79
James <logicism81@gmail.com>
2024-03-30 05:30:11 -0400

    minor updates

--- src/lib/contentful.tsx
+++ src/lib/contentful.tsx
@@ -12,8 +12,13 @@ export type PageDataType = {
 };
 
 export async function fetchPages() {
-  const res = await client.getEntries();
-  return res.items.map((item) => item.fields) as PageDataType[];
+  const res = await client.getEntries({ content_type: "navLinks" });
+  if (res.items.length === 0) throw new Error("No pages found");
+
+  const raw = res.items[0].fields.links as { fields: PageDataType }[];
+  if (!raw) throw new Error("No links found");
+
+  return raw.map((item) => item.fields) as PageDataType[];
 }
 
 export default client;
