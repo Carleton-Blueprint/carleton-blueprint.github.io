d708f05d453acbddcbfa68365240773798185cf8
heyitsok <omark212378@gmail.com>
2024-04-08 18:18:57 -0400

    Connected Contentful data to webpage

--- src/app/(pages)/contact/_components/ContactCard.tsx
+++ src/app/(pages)/contact/_components/ContactCard.tsx
@@ -1,5 +1,10 @@
 import { StaticImageData } from "next/image";
 import React, { ReactElement } from "react";
+import { ContactCardDataType } from "@/lib/contentful";
+import { FaDiscord } from "react-icons/fa";
+import { GrInstagram } from "react-icons/gr";
+import { MdOutlineAlternateEmail } from "react-icons/md";
+import { FaLinkedinIn } from "react-icons/fa";
 import {
     Card,
     CardContent,
@@ -7,14 +12,19 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
-  
 
-type ContactCardDataType = {
-    title: string;
-    description: string;
-    icon: ReactElement;
-    link: string;
-};
+enum IconType {
+    discord = "discord",
+    instagram = "instagram",
+    email = "email",
+}
+
+const iconElements = {
+    "discord": <FaDiscord className="size-32 text-blueprint"/>,
+    "instagram": <GrInstagram className="size-32 text-blueprint"/>,
+    "email": <MdOutlineAlternateEmail className="size-32 text-blueprint"/>,
+    "linkedin": <FaLinkedinIn className="size-32 text-blueprint"/>,
+}
 
 export default function ContactCard({data}: {data: ContactCardDataType}) {
   return (
@@ -22,7 +32,7 @@ export default function ContactCard({data}: {data: ContactCardDataType}) {
         <a href={data.link}>
         <Card className="ease-out duration-300 hover:drop-shadow-xl pt-16 max-w-96">
             <CardContent className="flex justify-center">
-                {data.icon}
+                {iconElements[data.icon as IconType] || <div>Icon not found</div>}
             </CardContent>
             <CardHeader className="text-center">
                 <CardTitle>{data.title}</CardTitle>
--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -1,27 +1,13 @@
 import React from "react";
 import ContactCard from "./_components/ContactCard";
 import { v4 as uuidv4 } from "uuid";
-import { FaDiscord } from "react-icons/fa";
-import { GrInstagram } from "react-icons/gr";
 import Image from "next/image";
 import image from "./_assets/Adult talking cell phone-rafiki.svg";
+import { fetchContact } from "@/lib/contentful";
 
-const contactData = [
-  {
-    title: "Discord",
-    description: "Join our Discord server to get involved with Blueprint!",
-    icon: <FaDiscord className="size-32 text-blueprint"/>,
-    link: "https://discord.com/invite/7gkkW5nJPW",
-  },
-  {
-    title: "Instagram",
-    description: "Follow us on Instagram to stay updated on our latest projects and events!",
-    icon: <GrInstagram className="size-32 text-blueprint"/>,
-    link: "https://www.instagram.com/cublueprint/",
-  }
-];
-
-export default function Contact() {
+export default async function Contact() {
+  const res = await fetchContact();
+  
   return (
     <div className="bg-blueprint">
       <div className="container flex flex-col justify-around">
@@ -36,15 +22,20 @@ export default function Contact() {
         </div>
       </div>
       <div className="sticky">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 80 1440 240">
-            <path fill="#aed9ff" fill-opacity="1" d="M1440,160L1380,176C1320,192,1200,224,1080,234.7C960,245,840,235,720,208C600,181,480,139,360,117.3C240,96,120,96,60,96L0,96L0,320L60,320C120,320,240,320,360,320C480,320,600,320,720,320C840,320,960,320,1080,320C1200,320,1320,320,1380,320L1440,320Z"></path>
-            <path fill="#4aa3ff" fill-opacity="1" d="M0,160L60,176C120,192,240,224,360,234.7C480,245,600,235,720,208C840,181,960,139,1080,117.3C1200,96,1320,96,1380,96L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 80 1440 180">
+            <path fill="#4aa3ff" fillOpacity="1" d="M1440,160L1380,176C1320,192,1200,224,1080,234.7C960,245,840,235,720,208C600,181,480,139,360,117.3C240,96,120,96,60,96L0,96L0,320L60,320C120,320,240,320,360,320C480,320,600,320,720,320C840,320,960,320,1080,320C1200,320,1320,320,1380,320L1440,320Z"></path>
+            <path fill="#1a89ff" fillOpacity="1" d="M0,160L60,176C120,192,240,224,360,234.7C480,245,600,235,720,208C840,181,960,139,1080,117.3C1200,96,1320,96,1380,96L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
           </svg>
       </div>
-      <hr />
+      <div className="sticky bg-white">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 80 1440 180" transform="scale(-1, -1)">
+            <path fill="#4aa3ff" fillOpacity="1" d="M1440,160L1380,176C1320,192,1200,224,1080,234.7C960,245,840,235,720,208C600,181,480,139,360,117.3C240,96,120,96,60,96L0,96L0,320L60,320C120,320,240,320,360,320C480,320,600,320,720,320C840,320,960,320,1080,320C1200,320,1320,320,1380,320L1440,320Z"></path>
+            <path fill="#1a89ff" fillOpacity="1" d="M0,160L60,176C120,192,240,224,360,234.7C480,245,600,235,720,208C840,181,960,139,1080,117.3C1200,96,1320,96,1380,96L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+          </svg>
+      </div>
       <div className="flex justify-center p-10 bg-white">
         <div className="w-2/4  flex justify-between flex-wrap">
-          {contactData.map((data) => (
+          {res.map((data) => (
             <ContactCard key={uuidv4()} data={data}/>
           ))}
         </div>
