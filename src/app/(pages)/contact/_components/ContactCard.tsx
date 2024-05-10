15ce26ca75d55eca855a58396bf03182e34a0b51
Omar Khan <omark212378@gmail.com>
2024-05-10 16:59:18 -0400

    Fixed card heights, mobile layout issues, and finishing touches

--- src/app/(pages)/contact/_components/ContactCard.tsx
+++ src/app/(pages)/contact/_components/ContactCard.tsx
@@ -20,17 +20,17 @@ enum IconType {
 }
 
 const iconElements = {
-    "discord": <FaDiscord className="size-32 text-blueprint-400"/>,
-    "instagram": <GrInstagram className="size-32 text-blueprint-400"/>,
-    "email": <MdOutlineAlternateEmail className="size-32 text-blueprint-400"/>,
-    "linkedin": <FaLinkedinIn className="size-32 text-blueprint-400"/>,
+    "discord": <FaDiscord className="size-24 text-blueprint-400"/>,
+    "instagram": <GrInstagram className="size-24 text-blueprint-400"/>,
+    "email": <MdOutlineAlternateEmail className="size-24 text-blueprint-400"/>,
+    "linkedin": <FaLinkedinIn className="size-24 text-blueprint-400"/>,
 }
 
 export default function ContactCard({data}: {data: ContactCardDataType}) {
   return (
     <div className="box-border mb-12">
         <a href={data.link}>
-        <Card className="ease-out duration-300 hover:drop-shadow-xl pt-10 max-w-64">
+        <Card className="ease-out duration-300 hover:drop-shadow-xl pt-10 max-w-64 min-h-full">
             <CardContent className="flex justify-center">
                 {iconElements[data.icon as IconType] || <div>Icon not found</div>}
             </CardContent>
