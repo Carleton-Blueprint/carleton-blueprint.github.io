7f15ad10abe6560cd2b1e46d70ace3ea13c072fe
Anniechika <79059777+Anniechika@users.noreply.github.com>
2024-05-24 22:38:23 -0400

    Update the event page with new changes

--- src/app/(pages)/events/_components/EventCard.tsx
+++ src/app/(pages)/events/_components/EventCard.tsx
@@ -5,23 +5,27 @@ interface EventCardProps {
     imgURL: string;
     title: string;
     venue: string;
+    time: string;
     description: string;
     isUpcoming: boolean;
+    linkUrl?: string;
 }
 
-const EventCard: React.FC<EventCardProps> = ({ imgURL, title, venue, description, isUpcoming }) => {
+const EventCard: React.FC<EventCardProps> = ({ imgURL, title, venue, time, description, isUpcoming, linkUrl }) => {
     return (
-        <div className="flex flex-col bg-white rounded-lg overflow-hidden shadow-lg transition-transform duration-200 mt-12 max-w-md w-full relative">
+        <a href={linkUrl} target="_blank" rel="noopener noreferrer" className="flex flex-col bg-white rounded-lg overflow-hidden shadow-md transition-transform duration-200 mt-12 max-w-md w-full relative hover:shadow-xl transform hover:-translate-y-1 cursor-pointer">
             <div className="relative w-full h-72">
                 <Image src={imgURL} alt={title} layout="fill" objectFit="cover" />
             </div>
-            <div className="p-6 flex flex-col items-center justify-center text-center">
-                <h2 className="text-xl font-bold mb-3 leading-tight">{title}</h2>
-                <p className="font-semibold text-gray-600">{venue}</p>
-                <p className="text-gray-500 text-sm mt-1">{description}</p>
+            <div className="p-6 flex flex-col items-center justify-center text-center"> 
+                <h2 className="text-xl font-bold mb-3">{title}</h2>
+                <p className="text-lg text-indigo-600 font-semibold">{venue}</p>
+                <p className="text-md italic text-gray-800">{time}</p>
+                <p className="text-gray-700">{description}</p>
                 {isUpcoming && <span className="px-3 py-1 text-white bg-green-500 rounded-full text-xs font-bold absolute top-4 right-4">Upcoming</span>}
+                {!isUpcoming && <span className="px-3 py-1 text-white bg-gray-300 rounded-full text-xs font-bold absolute top-4 right-4">Passed</span>}
             </div>
-        </div>
+        </a>
     );
 };
 
--- src/app/(pages)/events/page.tsx
+++ src/app/(pages)/events/page.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
-import EventCard from "./_components/EventCard";  // Assuming EventCard is in the same directory
+import EventCard from "./_components/EventCard";
 
 const EventsPage: React.FC = () => {
   return (
     <div className="container mx-auto p-4" style={{ paddingTop: '70px' }}>
-      {/* Added bg-blue-500 and text-white classes to h2 */}
-      <h2 className="text-2xl font-bold text-center mb-6 bg-blue-500 text-white py-2 rounded">
-        Upcoming Events
+      <h2 className="text-2xl font-bold text-center bg-blue-500 text-white px-12 py-3 pt-3 rounded-full w-full mb-0">
+        All Events
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <EventCard 
-            imgURL="/assets/project_night.png"
-            title="2024 Project Night"
-            venue="HP 5345, Monday (March 25th), 6pm"
-            description="Join us for an epic Trivia Night hosted by Blueprint! Test your knowledge and meet new friends 🙌"
-            isUpcoming={false}
-        />
-        <EventCard 
             imgURL="/assets/social_triva_night.png"
             title="Trivia Night 2023"
-            venue="HP 3431, Friday (April 5th), 6pm"
-            description="**Got a project you've been working on? Want to work on a project with others?**
-        Join us on our first Side Project Night to showcase your project, look for teammates, or network with others!"
-            isUpcoming={true}
+            time='Monday (March 25th), 6pm'
+            venue="HP 3431"
+            description="Got a project you've been working on? Want to work on a project with others? Join us!"
+            isUpcoming={false}
+            linkUrl="https://cublueprint.notion.site/f988151abd6448ebb70053c5ca1278f9?v=2042106f266f42d09c6d3357de17731b&p=a761217c557f467a9fa0ebadbf7ed033&pm=c"
+        />
+        <EventCard 
+            imgURL="/assets/project_night.png"
+            title="2024 Project Night"
+            venue="HP 5345"
+            time = "Friday (April 5th), 6pm"
+            description="Join us for an epic Trivia Night hosted by Blueprint! Test your knowledge and meet new friends 🙌"
+            isUpcoming={false}
+            linkUrl="https://cublueprint.notion.site/f988151abd6448ebb70053c5ca1278f9?v=2042106f266f42d09c6d3357de17731b&p=432e19e2e97643e7bec0ef52d3ce13ad&pm=c"
         />
       </div>
     </div>
