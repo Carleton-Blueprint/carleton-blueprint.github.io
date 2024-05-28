3a02cc75af5451dbea9a8fc093365f227131020e
Omar Khan <omark212378@gmail.com>
2024-05-28 18:07:20 -0400

    fixed team name filtering and component prop type

--- src/app/(pages)/students/_components/TeamProfile.tsx
+++ src/app/(pages)/students/_components/TeamProfile.tsx
@@ -1,27 +1,43 @@
-import Image from 'next/image'
-import { StudentDataType } from '@/lib/notion/students';
+import Image from "next/image";
 import defaultPicture from "../_assets/default.png";
 
-function TeamProfileBase ({ name, team, role, imageUrl, personalUrl }: StudentDataType){
+type TeamProfileProps = {
+  name: string;
+  role: string;
+  team?: string;
+  imageUrl: string;
+  personalUrl?: string;
+};
+
+function TeamProfileBase({ name, role, imageUrl }: TeamProfileProps) {
   return (
     <div className="flex flex-col items-center text-center space-y-1">
-        <div className="rounded-full overflow-hidden w-32 h-32 mb-2">
-          <Image src={imageUrl == "No URL" ? defaultPicture : imageUrl} width={128} height={128} alt="Picture of Jalal Bouri"/>
-        </div>
-          <p className="font-bold">{name}</p>
-          <p className="text-charcoal-500 capitalize">{role}</p>
+      <div className="rounded-full overflow-hidden w-32 h-32 mb-2">
+        <Image
+          src={imageUrl == "No URL" ? defaultPicture : imageUrl}
+          width={128}
+          height={128}
+          alt="Picture of Jalal Bouri"
+        />
+      </div>
+      <p className="font-bold">{name}</p>
+      <p className="text-charcoal-500 capitalize">{role}</p>
     </div>
-  )
+  );
 }
 
-export default function TeamProfile({ name, team, role, imageUrl, personalUrl }: StudentDataType) {
-  if(personalUrl == "No URL"){
+export default function TeamProfile({
+  name,
+  role,
+  imageUrl,
+  personalUrl,
+}: TeamProfileProps) {
+  if (personalUrl == "No URL") {
+    return <TeamProfileBase name={name} role={role} imageUrl={imageUrl} />;
+  } else
     return (
-    <TeamProfileBase name={name} team={team} role={role} imageUrl={imageUrl} personalUrl={personalUrl}/>
-  )
-  } else return (
-    <a href={personalUrl} target="_blank">
-      <TeamProfileBase name={name} team={team} role={role} imageUrl={imageUrl} personalUrl={personalUrl}/>
-    </a>
-  )
+      <a href={personalUrl} target="_blank">
+        <TeamProfileBase name={name} role={role} imageUrl={imageUrl} />
+      </a>
+    );
 }
--- src/app/(pages)/students/page.tsx
+++ src/app/(pages)/students/page.tsx
@@ -1,17 +1,12 @@
 import TeamProfile from "./_components/TeamProfile";
-import getStudents from "@/lib/notion/students"
+import getStudents from "@/lib/notion/students";
 
 export default async function Students() {
+  const students = await getStudents();
 
-  const students = await getStudents()
+  const executives = students.filter((student) => student.team == "Executive");
 
-  const executives = students.filter((student) => (
-    student.team == "executive"
-  ))
-
-  const internals = students.filter((student) => (
-    student.team == "internal"
-  ))
+  const internals = students.filter((student) => student.team == "Internal");
 
   return (
     <div>
@@ -20,30 +15,46 @@ export default async function Students() {
           <section className="content container">
             <div className="flex flex-col space-y-2">
               <div className="mb-8">
-                <h2 className="text-4xl md:text-6xl font-extrabold mb-8 text-center pt-6">Meet the Team</h2>
-                <p className="text-lg text-center">Our community specializes in a variety of disciplines, but our passion for helping nonprofits and our interest in technology brings us together.</p>
+                <h2 className="text-4xl md:text-6xl font-extrabold mb-8 text-center pt-6">
+                  Meet the Team
+                </h2>
+                <p className="text-lg text-center">
+                  Our community specializes in a variety of disciplines, but our
+                  passion for helping nonprofits and our interest in technology
+                  brings us together.
+                </p>
               </div>
               <div className="flex flex-col space-y-24 text-center">
                 <div className="flex flex-col space-y-6">
-                  <h4 className="text-2xl md:text-4xl text-white bg-blueprint font-semibold my-4 p-3 rounded-md">Executive Team</h4>
+                  <h4 className="text-2xl md:text-4xl text-white bg-blueprint font-semibold my-4 p-3 rounded-md">
+                    Executive Team
+                  </h4>
                   <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-8">
                     {executives.map((person) => (
-                      <TeamProfile name={person.name} team={person.team} role={person.role} imageUrl={person.imageUrl} personalUrl={person.personalUrl} key={person.name}/>
+                      <TeamProfile
+                        name={person.name}
+                        role={person.role}
+                        imageUrl={person.imageUrl}
+                        personalUrl={person.personalUrl}
+                        key={person.name}
+                      />
                     ))}
                   </div>
                 </div>
                 <div className="flex flex-col space-y-6">
-                  <h4 className="text-2xl md:text-4xl text-white bg-blueprint  font-semibold my-4 p-3 rounded-md">Tech Team</h4>
+                  <h4 className="text-2xl md:text-4xl text-white bg-blueprint  font-semibold my-4 p-3 rounded-md">
+                    Tech Team
+                  </h4>
                   <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-8">
                     {internals.map((person) => (
-                      <TeamProfile 
+                      <TeamProfile
                         name={person.name}
                         team={person.team}
                         role={person.role}
                         imageUrl={person.imageUrl}
                         personalUrl={person.personalUrl}
                         key={person.name}
-                        />
+                      />
                     ))}
                   </div>
                 </div>
@@ -53,5 +64,5 @@ export default async function Students() {
         </div>
       </div>
     </div>
-  )
+  );
 }
