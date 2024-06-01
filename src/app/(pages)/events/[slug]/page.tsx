135f2b0d6d904d89795fdd7dca136818d9c340a8
Omar Khan <omark212378@gmail.com>
2024-06-01 14:52:03 -0400

    added generateStaticParams to events page slug

--- src/app/(pages)/events/[slug]/page.tsx
+++ src/app/(pages)/events/[slug]/page.tsx
@@ -1,7 +1,17 @@
 import { NotionRenderer } from "react-notion";
 import { Separator } from "@/components/ui/separator";
+import { EventDataType } from "@/lib/notion/events";
+import getEventPageIds from "@/lib/notion/events";
 
-const getAllPosts = async (NOTION_BLOG_ID: string) => {
+export async function generateStaticParams() {
+  const events = await getEventPageIds();
+
+  return events.map((event: EventDataType) => ({
+    slug: event.eventPageId,
+  }));
+}
+
+const getBlockMap = async (NOTION_BLOG_ID: string) => {
   return await fetch(
     `https://notion-api.splitbee.io/v1/page/${NOTION_BLOG_ID}`,
     { next: { revalidate: 30 } }
@@ -13,15 +23,15 @@ export default async function EventPage({
 }: {
   params: { slug: string };
 }) {
-  const allPosts = await getAllPosts(params.slug); //staticProps.props.posts;
-  const title = (Object.values(allPosts) as any)[0].value.properties
+  const blockMap = await getBlockMap(params.slug); //staticProps.props.posts;
+  const title = (Object.values(blockMap) as any)[0].value.properties
     .title[0][0];
   return (
     <div className="flex justify-center pt-12">
       <div className="w-1/2 space-y-8 border-l-4 pl-12 border-slate-200">
         <h1 className="text-4xl font-bold">{title}</h1>
         <Separator />
-        <NotionRenderer blockMap={allPosts} />
+        <NotionRenderer blockMap={blockMap} />
       </div>
     </div>
   );
