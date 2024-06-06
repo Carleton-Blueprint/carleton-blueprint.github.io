7140dc8037e5c22be531a9037c2f015f2fc0b3f9
Omar Khan <omark212378@gmail.com>
2024-06-06 11:47:03 -0600

    removed projects loading and updated deploy workflow to run every morning

--- src/app/(pages)/projects/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Skeleton } from "@/components/ui/skeleton";
-
-export default function Loading() {
-  return (
-    <div className="flex items-center flex-col my-16 mb-24">
-      <Skeleton className="w-[600px] h-[70px] rounded-full bg-slate-300" />
-    </div>
-  );
-}
