4ba25a623bb8321b1fdaf2fba2fd9e392d5eb4ca
Omar Khan <omark212378@gmail.com>
2024-06-06 12:02:16 -0600

    reduced getProjects() calls from two to one

--- src/app/(pages)/projects/_components/CurrentProjects.tsx
+++ src/app/(pages)/projects/_components/CurrentProjects.tsx
@@ -1,19 +1,29 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { v4 as uuidv4 } from "uuid";
 import ProjectCard from "./ProjectCard";
-import { getProjects, isCurrentProject } from "@/lib/notion/projects";
+import { ProjectDataType } from "@/lib/notion/projects";
 
-export default async function CurrentProjects() {
-  const res = await getProjects();
-  const projects = res.filter((project) => isCurrentProject(project));
-
+export default async function CurrentProjects({
+  projects,
+}: {
+  projects: ProjectDataType[];
+}) {
   return (
     <div className="flex items-center flex-col my-16 mb-24">
-      <h1 className="text-4xl md:text-6xl font-extrabold mb-8">Current Projects</h1>
-      <Tabs defaultValue={projects[0].companyName} className="hidden md:flex flex-col">
+      <h1 className="text-4xl md:text-6xl font-extrabold mb-8">
+        Current Projects
+      </h1>
+      <Tabs
+        defaultValue={projects[0].companyName}
+        className="hidden md:flex flex-col"
+      >
         <TabsList className="bg-blueprint-400 text-white mx-auto w-fit mb-5">
           {projects.map((project) => (
-            <TabsTrigger key={uuidv4()} className="px-10 hover:bg-blueprint-300 ease-in-out transition-all duration-500" value={project.companyName}>
+            <TabsTrigger
+              key={uuidv4()}
+              className="px-10 hover:bg-blueprint-300 ease-in-out transition-all duration-500"
+              value={project.companyName}
+            >
               {project.companyName}
             </TabsTrigger>
           ))}
--- src/app/(pages)/projects/_components/PastProjects.tsx
+++ src/app/(pages)/projects/_components/PastProjects.tsx
@@ -1,19 +1,19 @@
 import { v4 as uuidv4 } from "uuid";
 import ProjectCard from "./ProjectCard";
-import { getProjects, isCurrentProject } from "@/lib/notion/projects";
+import { ProjectDataType } from "@/lib/notion/projects";
 
-export default async function PastProjects() {
-  const projects = await getProjects();
-
+export default async function PastProjects({
+  projects,
+}: {
+  projects: ProjectDataType[];
+}) {
   return (
     <div className="flex items-center flex-col my-16">
       <h1 className="text-4xl font-bold mb-8">Past Projects</h1>
       <div className="flex flex-col items-center space-y-5">
-        {projects
-          .filter((project) => !isCurrentProject(project))
-          .map((project) => (
-            <ProjectCard key={uuidv4()} data={project} grid />
-          ))}
+        {projects.map((project) => (
+          <ProjectCard key={uuidv4()} data={project} grid />
+        ))}
       </div>
     </div>
   );
