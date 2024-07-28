1af9b36eaa06d72f71f7eddb4672e92dbe76044f
Omar Khan <omark212378@gmail.com>
2024-07-28 15:34:57 -0400

    changed a few colors and sizes

--- src/app/(pages)/students/_components/Section.tsx
+++ src/app/(pages)/students/_components/Section.tsx
@@ -1,22 +1,25 @@
-import { StudentDataType } from "@/lib/notion/students"
-import TeamProfile from "./TeamProfile"
-import BlockContainer from "../../home/_components/BlockContainer"
-import { v4 as uuidv4 } from "uuid"
+import { StudentDataType } from "@/lib/notion/students";
+import TeamProfile from "./TeamProfile";
+import BlockContainer from "../../home/_components/BlockContainer";
+import { v4 as uuidv4 } from "uuid";
 
 export type TeamDataType = {
-    teamName: string,
-    teamMembers: StudentDataType[]
-}
+  teamName: string;
+  teamMembers: StudentDataType[];
+};
 
-export default function Section({team}: {team: TeamDataType}) {
-    return (
-        <BlockContainer roundedCorners={true} shadow={true}>
-            <h2 className="py-2 font-bold text-4xl md:text-5xl text-left">{team.teamName + " team"}</h2>
-            <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
-                {team.teamMembers.map(person => (
-                    <TeamProfile student={person} key={uuidv4()}/>
-                ))}
-            </div>
-        </BlockContainer>
-    )
+export default function Section({ team }: { team: TeamDataType }) {
+  return (
+    <BlockContainer
+      title={team.teamName + " team"}
+      roundedCorners={true}
+      shadow={true}
+    >
+      <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
+        {team.teamMembers.map((person) => (
+          <TeamProfile student={person} key={uuidv4()} />
+        ))}
+      </div>
+    </BlockContainer>
+  );
 }
