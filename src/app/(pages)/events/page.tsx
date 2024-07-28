48af0c35f8f8bd68ed915fabeb6913b2daed37b1
Anniechika <79059777+Anniechika@users.noreply.github.com>
2024-07-28 17:16:48 -0400

    Use block container to redesign event page

--- src/app/(pages)/events/page.tsx
+++ src/app/(pages)/events/page.tsx
@@ -1,34 +1,36 @@
 import React from "react";
 import EventCard from "./_components/EventCard"; // Assuming EventCard is in the same directory
 import getEvents from "@/lib/notion/events";
+import BlockContainer from "../home/_components/BlockContainer";
 import blueprint from "./_assets/eventPlaceholder.jpeg";
 
 const EventsPage: React.FC = async () => {
   const res = await getEvents();
   return (
-    <div className="container mx-auto p-4 pt-[70px]">
-      <div className="text-center mb-4">
-        <h2 className="text-4xl font-bold text-blue-500 px-12 py-3 pt-3 rounded-full w-full mb-0">
-          Blueprints Events
-        </h2>
-        <hr className="w-1/2 border-t-4 border-blue-500 my-4 mx-auto" />
-      </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {res.map((event) => (
-            <EventCard
-              id={event.eventPageId}
-              key={event.eventName}
-              imgURL={event.coverURL}
-              title={event.eventName}
-              venue={event.venue}
-              time={event.date}
-              description={event.description}
-              isUpcoming={event.status === "Scheduled"}
-            />
-          ))}
+    <div className="bg-blueprint-50 flex justify-center">
+          <BlockContainer title="Blueprints Events" roundedCorners mobile_padding centered margin>
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 px-4 sm:px-16 lg:px-12">
+              {res.map((event) => (
+                <EventCard
+                  id={event.eventPageId}
+                  key={event.eventName}
+                  imgURL={event.coverURL}
+                  title={event.eventName}
+                  venue={event.venue}
+                  time={event.date}
+                  description={event.description}
+                  isUpcoming={event.status === "Scheduled"}
+                />
+              ))}
+            </div>
+          </BlockContainer>
         </div>
-    </div>
   );
 };
 
 export default EventsPage;
+
+
+//       <h2 className="text-4xl font-bold text-blue-500 px-12 py-3 pt-3 rounded-full w-full mb-0">
+          //Blueprints Events
+        //</h2>
--- src/app/(pages)/home/_components/BlockContainer.tsx
+++ src/app/(pages)/home/_components/BlockContainer.tsx
@@ -4,6 +4,7 @@ type Props = {
   flip?: boolean;
   roundedCorners?: "top" | "bottom" | true | false;
   centered?: boolean;
+  mobile_padding?: boolean;
   inner?: boolean;
   margin?: "top" | "bottom" | true | false;
   padding?: "title only" | true | false;
@@ -15,31 +16,31 @@ export default function BlockContainer({
   flip = false,
   roundedCorners = false,
   centered = false,
+  mobile_padding = false,
   inner = false,
   margin = false,
   padding = true,
 }: Props) {
   return (
     <div
-      className={`py-16
-        ${flip && "bg-blueprint-50"}
+      className={`py-16 w-screen
+        ${flip ? "bg-blueprint-50" : "bg-white"}
         ${roundedCorners === true && "rounded-[50px]"}
         ${roundedCorners === "top" && "rounded-t-[50px]"}
         ${roundedCorners === "bottom" && "rounded-b-[50px]"}
-        ${inner && "md:w-[90%] self-center"}
+        ${mobile_padding && "md:w-fit self-center px-4"}
+        ${inner && "md:w-fit self-center px-10"}
         ${margin === true && "my-12"}
         ${margin === "top" && "mt-12"}
         ${margin === "bottom" && "mb-12"}`}
     >
       <div
-        className={`space-y-12 
-        ${centered && "flex flex-col items-center"}
-        ${padding === true && "container"}`}
+        className={`space-y-2 container
+        ${centered && "flex flex-col items-center"}`}
       >
         {title && (
           <div
             className={`flex text-blueprint py-2 font-bold text-5xl md:text-6xl
-            ${padding === "title only" && !centered && "container"}
             ${centered && "text-center"}`}
           >
             {title}
