aafae44ec4fdda54d95bce86d2515bd7f15b041a
Omar Khan <omark212378@gmail.com>
2024-08-25 19:41:59 -0400

    fixed toast issues

--- src/app/(pages)/contact/_components/TriggerToast.tsx
+++
@@ -1,9 +0,0 @@
-import { useToast } from '@/components/ui/use-toast';
-export default function triggertoast(status: boolean) {
-  const { toast } = useToast();
-  if (status) {
-    toast({ title: 'Success', description: 'success' });
-  } else {
-    toast({ title: 'Error', description: 'error' });
-  }
-};
