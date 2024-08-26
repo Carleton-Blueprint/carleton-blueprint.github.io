98cc1a67f39e254e577f5667b064bd3b517d59b4
James <contact@jamesyap.org>
2024-08-25 20:00:49 -0400

    links open in new tab

--- src/app/(pages)/contact/_components/ContactCard.tsx
+++ src/app/(pages)/contact/_components/ContactCard.tsx
@@ -7,7 +7,7 @@ type ContactCardProps = {
 
 export default function ContactCard({ children, link = '' }: ContactCardProps) {
   return (
-    <a href={link}>
+    <a href={link} target='_blank' rel='noopener noreferrer'>
       <div className='box-border mb-12 bg-white w-72 h-72 rounded-[50px] flex flex-col items-center py-5 space-y-5 justify-center hover:shadow-2xl transition-shadow ease-in-out duration-200'>
         {children}
       </div>
