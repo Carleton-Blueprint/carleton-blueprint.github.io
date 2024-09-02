2357ae8d40a41788ce263ddf9c8ea9283cb5ca2f
Omar Khan <42219936+heyitsOK@users.noreply.github.com>
2024-09-02 19:17:16 -0400

    [fix] [feature-touchup] touched up some of the design, and fixed a problem on the events page (Omar) (#62)
    * [fix] reenbaled contact form functionality and changed email link
    
    * [fix] added ig and li links on contact page, shadow on students page, fixed width on blockcontainer inner prop, and updated env example
    
    * [fix] [feature-touchups] see PR for changelog

--- src/app/(pages)/students/page.tsx
+++ src/app/(pages)/students/page.tsx
@@ -15,22 +15,22 @@ export default async function Students() {
   const teamNamesArray = Array.from(teamNamesSet);
   const teams: TeamDataType[] = [];
   for (const teamName of teamNamesArray) {
-    const teamMembers = students.filter(student => student.team == teamName);
+    const teamMembers = students.filter((student) => student.team == teamName);
     teams.push({ teamName, teamMembers });
   }
 
   return (
-    <div className='bg-blueprint-50 min-h-screen overflow-x-hidden'>
-      <div className='flex flex-col space-y-24 relative pb-24 content container'>
-        <div className='flex flex-col space-y-2'>
-          <div className='m-8 flex flex-row items-center gap-5 justify-center md:justify-start'>
-            <h1 className='text-4xl md:text-5xl font-extrabold mb-8 pt-6'>
-              Meet our <span className='text-blueprint-500'>team</span>
+    <div className="bg-blueprint-50 min-h-screen overflow-x-hidden">
+      <div className="flex flex-col space-y-24 relative pb-24 content container">
+        <div className="flex flex-col space-y-2">
+          <div className="m-8 flex flex-row items-center gap-5 justify-center md:justify-start">
+            <h1 className="text-4xl md:text-5xl font-extrabold mb-8 pt-6">
+              Meet our <span className="text-blueprint-500">Team</span>
             </h1>
-            <Image src={bluePeople} width={188.5} alt='Image of blue figures' />
+            <Image src={bluePeople} width={188.5} alt="Image of blue figures" />
           </div>
-          <div className='flex flex-col space-y-24 text-center'>
-            {teams.map(team => (
+          <div className="flex flex-col space-y-24 text-center">
+            {teams.map((team) => (
               <Section team={team} key={uuidv4()} />
             ))}
           </div>
