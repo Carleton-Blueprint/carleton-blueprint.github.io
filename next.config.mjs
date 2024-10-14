dd21976996a35e1815b726fa9bdbbdbb75fb836f
James <contact@jamesyap.org>
2024-10-14 14:02:42 -0400

    [feat] added dockerfile

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,14 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
+  output: 'standalone',
 };
 
 export default nextConfig;
