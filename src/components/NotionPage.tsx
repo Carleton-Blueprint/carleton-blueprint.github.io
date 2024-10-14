479742d746016395eb6fadcb5fae873f9516988b
James <contact@jamesyap.org>
2024-10-14 13:50:13 -0400

    fix omar's bugs

--- src/components/NotionPage.tsx
+++ src/components/NotionPage.tsx
@@ -21,40 +21,34 @@ type PropsType = {
   eventDetails?: EventDetailsType;
 };
 
-export function NotionPage({ recordMap, title, eventDetails }: PropsType) {
+export default function NotionPage({ recordMap, title, eventDetails }: PropsType) {
   return (
-    <div className="flex flex-col items-center justify-center py-12 bg-blue-50">
-      <h1 className="text-5xl text-blueprint font-bold mb-10 text-center">
-        {title}
-      </h1>
+    <div className='flex flex-col items-center justify-center py-12 bg-blue-50'>
+      <h1 className='text-5xl text-blueprint font-bold mb-10 text-center'>{title}</h1>
 
       <BlockContainer roundedCorners centered>
-        <div className="px-[96px] w-full">
+        <div className='px-[96px] w-full'>
           {eventDetails && (
-            <div className="mb-10">
-              <h1 className="text-3xl text-gray-800 font-semibold mb-5">
-                Event Details
-              </h1>
-              <div className="grid grid-cols-2 gap-2 w-[500px] items-center">
-                <p className="flex gap-1 items-center text-md text-gray-800">
+            <div className='mb-10'>
+              <h1 className='text-3xl text-gray-800 font-semibold mb-5'>Event Details</h1>
+              <div className='grid grid-cols-2 gap-2 w-[500px] items-center'>
+                <p className='flex gap-1 items-center text-md text-gray-800'>
                   <BsCalendar3 /> Date
                 </p>
-                <p className="text-md text-gray-800">{eventDetails.date}</p>
-                <p className="flex gap-1 items-center text-md text-gray-800">
+                <p className='text-md text-gray-800'>{eventDetails.date}</p>
+                <p className='flex gap-1 items-center text-md text-gray-800'>
                   <MdOutlineLocationCity /> Venue
                 </p>
-                <p className="text-md text-gray-800">{eventDetails.venue}</p>
-                <p className="flex gap-1 items-center text-md text-gray-800">
+                <p className='text-md text-gray-800'>{eventDetails.venue}</p>
+                <p className='flex gap-1 items-center text-md text-gray-800'>
                   <MdOutlineUpcoming /> Status
                 </p>
                 {eventDetails.status === 'Scheduled' ? (
-                  <span className="px-3 py-1 text-white bg-green-500 rounded-full text-xs font-bold w-fit">
+                  <span className='px-3 py-1 text-white bg-green-500 rounded-full text-xs font-bold w-fit'>
                     Upcoming
                   </span>
                 ) : (
-                  <span className="px-3 py-1 text-white bg-gray-400 rounded-full text-xs font-bold w-fit">
-                    Passed
-                  </span>
+                  <span className='px-3 py-1 text-white bg-gray-400 rounded-full text-xs font-bold w-fit'>Passed</span>
                 )}
               </div>
             </div>
