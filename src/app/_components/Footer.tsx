4ce94ad988b44c25226e7e96199087d896d61935
James <yapsh85@gmail.com>
2024-10-15 01:55:39 -0400

    [chore] testing deployment

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -38,7 +38,7 @@ export default async function Footer() {
             </Link>
             <div className='w-2/3 text-end'>Made with ❤️ by</div>
             <div className='w-2/3 md:w-full text-right'>
-              Carleton Blueprint @ Carleton University
+              Blueprint @ Carleton University
               <br /> Ottawa, Ontario, Canada
             </div>
           </div>
