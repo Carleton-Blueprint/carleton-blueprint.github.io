d023ffa3cefe33a643b5c88eb54a3ca3c5e985c9
James <contact@jamesyap.org>
2024-10-15 21:22:38 -0400

    Disabled Resend

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,26 +1,30 @@
-import { EmailTemplate } from '@/app/(pages)/contact/_components/email-template';
-import { Resend } from 'resend';
+// import { EmailTemplate } from '@/app/(pages)/contact/_components/email-template';
+// import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
   const data = await request.json();
-  const { name, email, message } = data;
-  try {
-    const { data, error } = await resend.emails.send({
-      from: 'Carleton Blueprint <onboarding@resend.dev>',
-      to: ['info@carletonblueprint.org'],
-      subject: 'Message from Blueprint Contact Form',
-      react: EmailTemplate({ name: name, email: email, message: message }),
-    });
+  // const { name, email, message } = data;
 
-    if (error) {
-      return NextResponse.json({ error }, { status: 500 });
-    }
+  // try {
+  //   const { data, error } = await resend.emails.send({
+  //     from: 'Carleton Blueprint <onboarding@resend.dev>',
+  //     to: ['info@carletonblueprint.org'],
+  //     subject: 'Message from Blueprint Contact Form',
+  //     react: EmailTemplate({ name: name, email: email, message: message }),
+  //   });
 
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
-  }
+  //   if (error) {
+  //     return NextResponse.json({ error }, { status: 500 });
+  //   }
+
+  //   return NextResponse.json(data);
+  // } catch (error) {
+  //   return NextResponse.json({ error }, { status: 500 });
+  // }
+
+  // echo response
+  return NextResponse.json(data);
 }
