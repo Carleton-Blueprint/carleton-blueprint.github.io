46874644c4cd0e49ca38e744b0e91b49c35dfc12
Omar Khan <omark212378@gmail.com>
2024-10-19 23:24:22 -0400

    [fix] removed blueprinters from projects page

--- src/app/(pages)/projects/page.tsx
+++ src/app/(pages)/projects/page.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import CurrentProjects from './_components/CurrentProjects';
 import PastProjects from './_components/PastProjects';
 import { getProjects, isCurrentProject } from '@/lib/notion/projects';
-import Image from 'next/image';
-import mission from './_assets/mission.svg';
-import culture from './_assets/culture.svg';
-import innovate from './_assets/innovate.svg';
-import personal from './_assets/personal.svg';
-import humble from './_assets/humble.svg';
 
 export default async function Projects() {
   const res = await getProjects();
@@ -17,19 +11,6 @@ export default async function Projects() {
   return (
     <div className="space-y-6">
       <CurrentProjects projects={currentProjects} />
-      <div className="flex justify-center">
-        <Image
-          src={mission}
-          alt="line"
-          width={150}
-          height={150}
-          className="mr-12"
-        />
-        <Image src={personal} alt="line" width={150} height={150} />
-        <Image src={culture} alt="line" width={150} height={150} />
-        <Image src={humble} alt="line" width={150} height={150} />
-        <Image src={innovate} alt="line" width={150} height={150} />
-      </div>
       <PastProjects projects={pastProjects} />
     </div>
   );
