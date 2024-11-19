0c414f6a336e61bd20eb189b32487913c3f340bc
Omar Khan <omark212378@gmail.com>
2024-11-18 21:12:04 -0500

    fixed issues with ServerCard.tsx

--- src/app/(pages)/home/_components/ServerCard.tsx
+++ src/app/(pages)/home/_components/ServerCard.tsx
@@ -1,17 +1,15 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
 import ServerProLogo from "../_assets/server_pro_logo.png";
-import BlockContainer from "@/components/BlockContainer";
 
 export default function ServerCard() {
   return (
-    <BlockContainer>
-      <Card className="bg-gradient-to-r from-blue-400 to-blue-500 rounded-[35px] md:w-full p-6 shadow-[2px_6px_4px_0_rgba(0,0,0,0.3)] flex items-center justify-between text-white w-[300px]">
-        <div className="p-6 pt-2">
+    <div className="flex justify-between text-white bg-gradient-to-r from-blueprint from-45% to-white to-85% -mx-20 rounded-[50px] p-6 pl-20 hover:shadow-[0px_4px_4px_0_rgba(0,0,0,0.25)] transition-shadow ease-in-out duration-300">
+        <div className="space-y-4 w-2/3 flex flex-col justify-center">
           <CardHeader className="p-0">
-            <CardTitle className="text-xl font-semibold">Server.pro</CardTitle>
+            <CardTitle className="text-5xl font-semibold">Server.pro</CardTitle>
           </CardHeader>
-          <CardContent className="p-0 text-sm">
+          <CardContent className="p-0 text-xl">
             <p>
               Some description of the sponsor here. Some description of the
               sponsor here. Some description of the sponsor here. Some
@@ -20,16 +18,13 @@ export default function ServerCard() {
             </p>
           </CardContent>
         </div>
-        <div className="ml-4">
-          <div className="bg-white p-4 rounded-full">
+        <div className="w-1/4 flex justify-end">
             <Image
-              className="w-10 h-10"
+              className="w-[175px] rounded-full"
               src={ServerProLogo}
               alt="Server.pro logo"
             />
-          </div>
         </div>
-      </Card>
-    </BlockContainer>
+      </div>
   );
 }
--- src/app/(pages)/home/_components/SponsorButton.tsx
+++ src/app/(pages)/home/_components/SponsorButton.tsx
@@ -1,6 +1,6 @@
 export default function SponsorButton() {
   return (
-    <button className="bg-blueprint hover:bg-blueprint-400 text-white py-2 px-4 rounded-full tracking-[0.3em] hover:tracking-[0.2em] transition-all ease-in-out float-right">
+    <button className="bg-blueprint hover:bg-blueprint-400 text-white py-2 px-4 rounded-full w-48">
       Become a Sponsor!
     </button>
   );
