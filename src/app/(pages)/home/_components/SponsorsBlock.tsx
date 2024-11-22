8f765805c5e2427caa1322518ede508f9969da81
Omar Khan <omark212378@gmail.com>
2024-11-22 01:08:47 -0500

    Fixed formatting and linting issues

--- src/app/(pages)/home/_components/SponsorsBlock.tsx
+++ src/app/(pages)/home/_components/SponsorsBlock.tsx
@@ -1,6 +1,6 @@
-import BlockContainer from "@/components/BlockContainer";
-import SponsorButton from "./SponsorButton";
-import ServerCard from "./ServerCard";
+import BlockContainer from '@/components/BlockContainer';
+import SponsorButton from './SponsorButton';
+import ServerCard from './ServerCard';
 
 export default function SponsorsBlock() {
   return (
--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,9 +1,9 @@
-import AboutUsBlock from "./_components/AboutUsBlock";
-import HeroBlock from "./_components/HeroBlock";
-import OurValuesBlock from "./_components/OurValuesBlock";
-import ProjectsBlock from "./_components/ProjectsBlock";
-import EventsBlock from "./_components/EventsBlock";
-import SponsorsBlock from "./_components/SponsorsBlock";
+import AboutUsBlock from './_components/AboutUsBlock';
+import HeroBlock from './_components/HeroBlock';
+import OurValuesBlock from './_components/OurValuesBlock';
+import ProjectsBlock from './_components/ProjectsBlock';
+import EventsBlock from './_components/EventsBlock';
+import SponsorsBlock from './_components/SponsorsBlock';
 
 export default async function Home() {
   return (
