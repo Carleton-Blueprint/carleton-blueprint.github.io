a1769fc3ed4401ac6f6afafe1271e655fbb534a2
Fatema Lokhandwala <fatemaldhd@gmail.com>
2024-11-21 22:53:36 -0500

    created additional sponsor

--- src/app/(pages)/home/_components/ServerCard.tsx
+++ src/app/(pages)/home/_components/ServerCard.tsx
@@ -3,28 +3,40 @@ import Image from "next/image";
 import ServerProLogo from "../_assets/server_pro_logo.png";
 
 export default function ServerCard() {
-  return (
+  let sponsors = [
+    {
+      name: "Server.pro",
+      description:
+        "Some description of the sponsor here. Some description of the sponsor here. Some description of the sponsor here. Some description of the sponsor here. Some description of the sponsor here.",
+      logo: ServerProLogo,
+    },
+    {
+      name: "Another Sponsor",
+      description:
+        "Some random descripton. Some random descripton. Some random descripton. Some random descripton. Some random descripton. Some random descripton. Some random descripton. Some random descripton.",
+      logo: ServerProLogo,
+    },
+  ];
+
+  return sponsors.map((sponsor) => (
     <div className="flex justify-between text-white bg-gradient-to-r from-blueprint from-45% to-white to-85% -mx-20 rounded-[50px] p-6 pl-20 hover:shadow-[0px_4px_4px_0_rgba(0,0,0,0.25)] transition-shadow ease-in-out duration-300">
-        <div className="space-y-4 w-2/3 flex flex-col justify-center">
-          <CardHeader className="p-0">
-            <CardTitle className="text-5xl font-semibold">Server.pro</CardTitle>
-          </CardHeader>
-          <CardContent className="p-0 text-xl">
-            <p>
-              Some description of the sponsor here. Some description of the
-              sponsor here. Some description of the sponsor here. Some
-              description of the sponsor here. Some description of the sponsor
-              here.
-            </p>
-          </CardContent>
-        </div>
-        <div className="w-1/4 flex justify-end">
-            <Image
-              className="w-[175px] rounded-full"
-              src={ServerProLogo}
-              alt="Server.pro logo"
-            />
-        </div>
+      <div className="space-y-4 w-2/3 flex flex-col justify-center">
+        <CardHeader className="p-0">
+          <CardTitle className="text-5xl font-semibold">
+            {sponsor.name}
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="p-0 text-xl">
+          <p>{sponsor.description}</p>
+        </CardContent>
       </div>
-  );
+      <div className="w-1/4 flex justify-end">
+        <Image
+          className="w-[175px] rounded-full"
+          src={sponsor.logo}
+          alt={`${sponsor.name} logo`}
+        />
+      </div>
+    </div>
+  ));
 }
--- src/app/(pages)/home/_components/SponsorButton.tsx
+++ src/app/(pages)/home/_components/SponsorButton.tsx
@@ -1,6 +1,6 @@
 export default function SponsorButton() {
   return (
-    <button className="bg-blueprint hover:bg-blueprint-400 text-white py-2 px-4 rounded-full w-48">
+    <button className="bg-blueprint hover:bg-blueprint-400 text-white py-2 px-4 rounded-full w-48 float-right">
       Become a Sponsor!
     </button>
   );
