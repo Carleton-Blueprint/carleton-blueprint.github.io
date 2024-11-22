15c51ee14b2b49b4a7bab2db5c112bebdb119a16
Fatema Lokhandwala <fatemaldhd@gmail.com>
2024-11-21 22:53:36 -0500

    created additional sponsor

--- src/app/(pages)/home/_components/SponsorsBlock.tsx
+++ src/app/(pages)/home/_components/SponsorsBlock.tsx
@@ -2,11 +2,11 @@ import BlockContainer from "@/components/BlockContainer";
 import SponsorButton from "./SponsorButton";
 import ServerCard from "./ServerCard";
 
-
 export default function SponsorsBlock() {
   return (
     <BlockContainer title="Our Sponsors" padding="less" inner margin="bottom">
       <ServerCard />
+      <SponsorButton />
     </BlockContainer>
   );
 }
