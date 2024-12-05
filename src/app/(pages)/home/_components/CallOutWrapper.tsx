8c316b45e987b7eaf2ab965dc6a20fd31c0c800f
Omar Khan <omark212378@gmail.com>
2024-12-05 00:36:49 -0500

    Dismounted component on transition end

--- src/app/(pages)/home/_components/CallOutWrapper.tsx
+++ src/app/(pages)/home/_components/CallOutWrapper.tsx
@@ -6,14 +6,24 @@ import { AnnouncementDataType } from '@/lib/notion/announcements';
 
 export default function CallOutWrapper({ text }: { text: AnnouncementDataType }) {
   const [isVisible, setIsVisible] = useState(true);
+  const [isRendered, setIsRendered] = useState(true);
 
   const toggleVisibility = () => {
-    setIsVisible(!isVisible);
+    setIsVisible(false);
   };
 
-  return (
-    <div className={`${isVisible ? 'opacity-100' : 'opacity-0'} transition-opacity duration-300 ease-in-out`}>
+  const handleTransitionEnd = () => {
+    if (!isVisible) {
+      setIsRendered(false); // Unmount component when transition ends
+    }
+  };
+
+  return isRendered ? (
+    <div
+      className={`${isVisible ? 'opacity-100' : 'opacity-0'} transition-opacity duration-300 ease-in-out`}
+      onTransitionEnd={handleTransitionEnd}
+    >
       {<CallOut text={text} toggleVisibility={toggleVisibility} />}
     </div>
-  );
+  ) : null;
 }
