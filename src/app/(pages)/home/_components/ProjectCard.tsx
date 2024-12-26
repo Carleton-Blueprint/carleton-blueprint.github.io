8e57b02190eb5aeb16f6b130e0216de00741b8aa
Joshua John <60818386+joshuajohn08@users.noreply.github.com>
2024-12-26 14:44:01 -0500

    Decreased padding and added line clamping for project card (#157)
    * decreased padding and added line clamping
    
    * reran with prettier
    
    * added line clamping for companyName

--- src/app/(pages)/home/_components/ProjectCard.tsx
+++ src/app/(pages)/home/_components/ProjectCard.tsx
@@ -16,8 +16,8 @@ export default function HomeProjectCard({ data }: { data: ProjectDataType }) {
         className="absolute md:w-80 md:h-80 lg:w-72 lg:h-72 rounded-[40px]"
       />
       <div className="group w-80 h-80 min-w-fit md:w-80 md:h-80 lg:w-72 lg:h-72 relative bg-gradient-to-t from-blueprint/70 to-blueprint/15 from-25% to-65% rounded-[35px]">
-        <span className="absolute bottom-0 inset-x-0 px-6 pb-10 text-center text-white translate-y-10 group-hover:translate-y-0 group:transition duration-300 ease-in-out">
-          <h2 className="text-3xl font-semibold">{data.companyName}</h2>
+        <span className="absolute bottom-0 inset-x-0 px-5 pb-10 text-center text-white translate-y-10 group-hover:translate-y-0 group:transition duration-300 ease-in-out">
+          <h2 className="text-3xl font-semibold line-clamp-2">{data.companyName}</h2>
           <p className="h-8 text-lg opacity-0 group-hover:opacity-100 group:transition duration-300 ease-in-out line-clamp-1">
             {data.productName}
           </p>
