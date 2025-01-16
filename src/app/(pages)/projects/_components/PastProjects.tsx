cd6c238962d86ac32cc45115718a149aa09a1213
Musa Ali <musa6ali@gmail.com>
2025-01-15 20:43:02 -0500

    Implement Projects page mobile view (#146)
    * feat: add mobile styles to projects page
    
    * chore: run prettier write
    
    * fix: hide past project github icon on non-mobile view
    
    * chore: run prettier write
    
    * fix: current projects spacing
    
    fix: fix fix
    
    * fix: past projects spacing
    
    fix: scale github icon with screen size
    
    ---------
    
    Co-authored-by: Omar Khan <42219936+heyitsOK@users.noreply.github.com>

--- src/app/(pages)/projects/_components/PastProjects.tsx
+++ src/app/(pages)/projects/_components/PastProjects.tsx
@@ -5,7 +5,7 @@ import BlockContainer from '@/components/BlockContainer';
 
 export default async function PastProjects({ projects }: { projects: ProjectDataType[] }) {
   return (
-    <BlockContainer title="Past Projects" roundedCorners="top" flip="dark-blue">
+    <BlockContainer title="Past Projects" roundedCorners="top" flip="dark-blue" padding="less" gap="less">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {projects.map(project => (
           <PastProjectCard key={uuidv4()} data={project} />
