9a656529b09f3d66673c9ddaec5f1ab91bb0d10b
Fatema Lokhandwala <fatemaldhd@gmail.com>
2025-03-15 17:57:09 -0400

    Implementing 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,22 +1,17 @@
-import Link from 'next/link';
+import WantedImage from './_assets/404_page_image.svg';
+import Image from 'next/image';
+import LinkButton from '@/components/LinkButton';
 
 export default function NotFound() {
+
   return (
-    <div className="flex min-h-[calc(100vh-300px)] items-center justify-center bg-blueprint-50 px-4">
-      <div className="w-full max-w-lg rounded-2xl bg-white p-8 text-center shadow-lg">
-        <h1 className="mb-4 text-4xl font-bold text-blueprint">404 - Page Not Found</h1>
-        <p className="mb-8 text-gray-600">
-          The page you&apos;re looking for doesn&apos;t exist. Please check the URL and try again.
-        </p>
-        <div className="space-x-4">
-          <Link
-            href="/"
-            className="inline-block rounded-full bg-gray-200 px-6 py-2 text-gray-800 transition-colors hover:bg-gray-300"
-          >
-            Go home
-          </Link>
-        </div>
-      </div>
+    <div className="bg-[#0A1E3A]">
+      <Image className="" src={WantedImage} alt={"Image of a Blueprinter looking at a Wanted poster"} />
+      <p className="font-[Poppins] font-[600] text-[96px] leading-[100%] tracking-[0%] text-[#FFFFFF]">404</p>
+      <p className="font-[Poppins] font-[600] text-[24px]">You found a pesky one!</p>
+      <LinkButton href="/" newTab={false} variant="filled">
+                Go back home
+      </LinkButton>
     </div>
   );
 }
