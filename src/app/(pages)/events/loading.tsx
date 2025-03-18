ffd5e6f2ee690a345c269dbbfbf66f0c779724f7
Omar Khan <omark212378@gmail.com>
2025-03-18 01:29:58 -0400

    [fix] formating

--- src/app/(pages)/events/loading.tsx
+++ src/app/(pages)/events/loading.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Card, CardContent } from '@/components/ui/card';
 import BlockContainer from '@/components/BlockContainer';
-import Image from 'next/image';
+// import Image from 'next/image';
 
 export default function Loading() {
   return (
@@ -16,7 +16,6 @@ export default function Loading() {
             >
               <div className="absolute inset-0"></div>
               <div className="absolute inset-0 rounded-[50px] bg-gradient-to-t from-gray-700/70 from-30% to-gray-500/20 to-70%"></div>
-
               <CardContent className="absolute bottom-0 left-0 w-full px-6 pb-8 text-white">
                 <div className="h-6 w-3/4 rounded bg-gray-500"></div>
                 <div className="mt-2 h-4 w-1/2 rounded bg-gray-600"></div>
