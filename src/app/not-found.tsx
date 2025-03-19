06e267028f2b0bbb5318cce83510dfdc19e47d60
Fatema Lokhandwala <fatemaldhd@gmail.com>
2025-03-18 23:50:29 -0400

    Adjusted spacing and changed color of text

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,15 +3,20 @@ import Image from 'next/image';
 import LinkButton from '@/components/LinkButton';
 
 export default function NotFound() {
-
   return (
-    <div className="bg-[#0A1E3A]">
-      <Image className="" src={WantedImage} alt={"Image of a Blueprinter looking at a Wanted poster"} />
-      <p className="font-[Poppins] font-[600] text-[96px] leading-[100%] tracking-[0%] text-[#FFFFFF]">404</p>
-      <p className="font-[Poppins] font-[600] text-[24px]">You found a pesky one!</p>
-      <LinkButton href="/" newTab={false} variant="filled">
-                Go back home
-      </LinkButton>
+    <div className="flex h-[calc(100vh-80px)] items-center justify-center bg-[#0A1E3A]">
+      <div className="flex items-center space-x-16">
+        <Image src={WantedImage} alt={'Image of a Blueprinter looking at a Wanted poster'} />
+        <div>
+          <p className="text-[96px] font-semibold leading-[100%] tracking-[0%] text-[#FFFFFF]">404</p>
+          <p className="text-[24px] font-semibold leading-[100%] tracking-[0%] text-[#FFFFFF]">
+            You found a pesky one!
+          </p>
+          <LinkButton href="/" newTab={false} variant="filled">
+            Go back home
+          </LinkButton>
+        </div>
+      </div>
     </div>
   );
 }
