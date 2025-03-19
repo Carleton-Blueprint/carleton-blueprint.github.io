67d08adf1ccbe83f3e2afb172116b901fe53698f
Omar Khan <omark212378@gmail.com>
2025-03-19 00:51:14 -0400

    [feat] Fixed mobile nav sidebar issues

--- src/app/_components/NavbarLinkItem.tsx
+++ src/app/_components/NavbarLinkItem.tsx
@@ -1,9 +1,20 @@
 import Link from 'next/link';
 import { PageDataType } from '@/lib/types';
+import { Dispatch, SetStateAction } from 'react';
 
-export default function NavbarLinkItem({ page }: { page: PageDataType }) {
+export default function NavbarLinkItem({
+  page,
+  setOpen,
+}: {
+  page: PageDataType;
+  setOpen?: Dispatch<SetStateAction<boolean>>;
+}) {
   return (
-    <Link href={page.href} className="text-lg text-white hover:text-blueprint-100">
+    <Link
+      href={page.href}
+      className="text-2xl text-white hover:text-blueprint-100 md:text-lg"
+      onClick={setOpen ? () => setOpen(prev => !prev) : undefined}
+    >
       {page.name}
     </Link>
   );
