bf58d060313d61a520bb0b38a06f351979f17d84
James <contact@jamesyap.org>
2025-03-30 21:26:05 -0400

    [refactor] Fix 'standalone' warning

--- src/app/(pages)/[notionId]/page.tsx
+++ src/app/(pages)/[notionId]/page.tsx
@@ -8,6 +8,7 @@ export async function generateStaticParams() {
 
 export default async function ArbitraryNotionPage({ params }: { params: Promise<{ notionId: string }> }) {
   const { notionId } = await params;
+
   const recordMap = await getRecordMap(notionId);
   const title = await getTitleByPageId(notionId);
 
