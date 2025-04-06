21438661f09a2cb33b5151e1a22846441793011a
AnirudhMahesh13 <109389814+AnirudhMahesh13@users.noreply.github.com>
2025-04-06 14:42:41 -0400

    remove routes.ts

--- src/app/(pages)/students/revalidate/route.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-import { NextResponse } from 'next/server';
-import { revalidatePath } from 'next/cache';
-
-export async function POST() {
-  revalidatePath('/students');
-  return NextResponse.json({ message: 'Hit endpoint', status: 200 });
-}
